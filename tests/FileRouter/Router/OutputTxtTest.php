6d06a33d3f850bef7b0c4eaf2f131de4f1ca5978
Pierre Klink <dev@klinks.info>
2013-02-19 22:19:53 +0100

    added tests

---
+++ tests/FileRouter/Router/OutputTxtTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace FileRouter\Router;
+
+class OutputTxtTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Load
+     */
+    protected $router;
+
+    /**
+     * @var \SplFileInfo
+     */
+    protected $sourcePath;
+
+    public function setUp()
+    {
+        $this->sourcePath = new \SplFileInfo(__DIR__. '/../../../example/txt');
+        $this->router = new OutputTxt($this->sourcePath);
+    }
+
+
+    public function testGetFileExtension()
+    {
+        // test default extension
+        $this->assertEquals('txt', $this->router->getFileExtension());
+    }
+
+
+    public function testHandleRoute()
+    {
+        $router = $this->router;
+
+        // handle non scalar route
+        try {
+            $router->handleRoute(array());
+            $this->assertTrue(false);
+        } catch (\UnexpectedValueException $e) {
+            $this->assertTrue(true);
+        }
+        try {
+            $router->handleRoute(new \stdClass());
+            $this->assertTrue(false);
+        } catch (\UnexpectedValueException $e) {
+            $this->assertTrue(true);
+        }
+
+        // handle non existing route
+        try {
+            $router->handleRoute('blblblbl');
+            $this->assertTrue(false);
+        } catch (\InvalidArgumentException $e) {
+            $this->assertTrue(true);
+        }
+
+        // existing file
+        ob_start();
+        $router->handleRoute('hello');
+        $this->assertEquals('hello!', ob_get_clean());
+
+        ob_start();
+        $router->handleRoute('hello/world');
+        $this->assertEquals('hello world!', ob_get_clean());
+    }
+
+}
