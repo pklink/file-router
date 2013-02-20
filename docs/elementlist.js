841133666b45bcd39ac799f64d8e08bf85760161
Pierre Klink <dev@klinks.info>
2013-02-20 15:11:00 +0100

    added API docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Exception"],["c","FileRouter\\Router"],["c","FileRouter\\Router\\AbstractImpl"],["c","FileRouter\\Router\\Load"],["c","FileRouter\\Router\\OutputTxt"],["c","InvalidArgumentException"],["c","LogicException"],["c","OutOfBoundsException"],["c","RuntimeException"],["c","SplFileInfo"],["c","UnexpectedValueException"]];
